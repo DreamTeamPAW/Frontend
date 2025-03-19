71efc79ecf8dd720c382c0bb5391cd78d849c21b
Dawid Klimkiewicz <82545462+dawidklimkiewicz@users.noreply.github.com>
2025-03-19 21:06:13 +0100

    Frontend-14: extracted style to a separate file

--- bookbox-frontend/src/app/page.tsx
+++ bookbox-frontend/src/app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <InputField className="w-s" placeholder="Login" icon={<User weight="fill" size={18}/>}/>
-      <InputField className="w-s" placeholder="No icon"/>
+      <InputField placeholder="Login" icon={<User weight="fill" size={18}/>}/>
         <Image
           className="dark:invert"
           src="/next.svg"
