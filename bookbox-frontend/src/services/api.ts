49dc86261626d04ba6932eb730330a61f531c3b9
ozarskiw <ozarskiw@gmail.com>
2025-03-21 13:38:48 +0100

    Token management in axios interceptors

--- bookbox-frontend/src/services/api.ts
+++ bookbox-frontend/src/services/api.ts
@@ -1,14 +1,27 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { API_URL } from './constants';
+import { clearToken, getToken } from '@/utils/token';
 
 const api: AxiosInstance = axios.create({
   baseURL: API_URL,
 });
 
+api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+        const token = getToken();
+        if (token) {
+            config.headers = config.headers || {};
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }
+);
+
 api.interceptors.response.use(
     (response: AxiosResponse) => response,
     (error: AxiosError) => {
         if (error.response?.status === 401) {
+            clearToken();
             // TODO: Redirect to login page
         }
         return Promise.reject(error);
