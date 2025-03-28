a6fff16e524d41bd402b5e6e97a4790af00ce2c3
ozarskiw <ozarskiw@gmail.com>
2025-03-28 10:32:05 +0100

    Book endpoints service

--- bookbox-frontend/src/services/constants.ts
+++ bookbox-frontend/src/services/constants.ts
@@ -3,10 +3,13 @@ export const LOGIN_URL = '/auth/login';
 export const REGISTER_URL = '/auth/register';
 export const LOGOUT_URL = '/auth/logout';
 export const ME_URL = '/auth/me';
+export const BOOKS_URL = '/api/books';
+
+
 
 export const AUTH_TOKEN_KEY = 'authToken';
 
-export enum UserRole{
+export enum UserRole {
     ADMIN = 'admin',
     USER = 'user'
 }
