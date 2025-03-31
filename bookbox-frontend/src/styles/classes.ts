9089f2df7fff28ae2b529b3d6e1c0ddf7453f989
Dawid Klimkiewicz <82545462+dawidklimkiewicz@users.noreply.github.com>
2025-04-01 00:18:18 +0200

    Frontend-14: added input field atom (#17)
    * Frontend-14: added input field atom
    
    * Frontend-14: extracted style to a separate file

---
+++ bookbox-frontend/src/styles/classes.ts
@@ -0,0 +1,6 @@
+export const inputFieldIconStyle = "absolute left-4 inset-y-0 flex items-center text-[var(--disabled-icon-color)] group-focus-within:text-[var(--text-input-color)]";
+
+export const inputFIeldStyle = `w-s py-2 pl-10 pr-4 rounded-xl bg-[var(--contrast-text-color)] transition focus:outline-none
+                border-1 border-[var(--disabled-border-color)] focus:border-[var(--focused-border-color)]
+                shadow-md focus:shadow-lg shadow-[var(--primary-shadow-color)] focus:shadow-[var(--secondary-shadow-color)]
+                text-[var(--text-input-color)] focus:text-[var(--primary-text-color)]`;
