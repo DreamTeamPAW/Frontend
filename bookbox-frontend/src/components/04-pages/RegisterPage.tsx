3400869919eacf67dd4517183ae6fb9a1fc9ca76
Adjest <128521507+mpadjest@users.noreply.github.com>
2025-04-13 23:40:45 +0200

    added scaling support
    need to finish edge cases (very zoomed in and very zoomed out)

--- bookbox-frontend/src/components/04-pages/RegisterPage.tsx
+++ bookbox-frontend/src/components/04-pages/RegisterPage.tsx
@@ -1,21 +1,52 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import RegisterTemplate from "../03-templates/RegisterTemplate";
 import RegisterForm from "../02-organisms/RegisterForm";
 import { backgroundImageLoginPage } from "@/styles/classes";
 
 const RegisterPage: React.FC = () => {
-    return (
-      <div style={backgroundImageLoginPage.backgroundContainer}>
-        <div className="relative h-screen w-screen overflow-x-hidden max-w-full">
+  const [scale, setScale] = useState(1);
+  const [translateX, setTranslateX] = useState(0);
+
+  useEffect(() => {
+    const handleZoom = () => {
+      const zoomLevel = window.innerWidth / window.outerWidth;
+
+      setScale(1/zoomLevel);
+
+      const newTranslateX = (zoomLevel - 1) * -500; 
+      setTranslateX(newTranslateX);
+    };
+    window.addEventListener("resize", handleZoom);
+    handleZoom();
+
+    return () => {
+      window.removeEventListener("resize", handleZoom);
+    };
+  }, []);
+
+  return (
+    <div style={backgroundImageLoginPage.backgroundContainer}>
+      <div className="relative h-screen w-screen overflow-hidden">
         <img
-        src="/images/bg1.png"
-        alt="Right Image"
-        draggable="false"
-        className="absolute right-0 top-0 w-[30vw] w-full h-[100vh] object-cover transform scale-x-[-1]"
-      />
-          <RegisterTemplate>
-            <RegisterForm />
-          </RegisterTemplate>
+          src="/images/bg1.png"
+          alt="Left Image"
+          draggable="false"
+          className="absolute inset-0 w-full h-full object-cover transition-transform duration-50"
+          style={{
+            transform: `scale(${-scale}) translateX(${translateX}px)`,
+          }}
+        />
+          <div
+            className="absolute inset-0 w-full h-full object-cover transition-transform duration-00"
+            style={{
+              transform: `translateX(${-translateX}px)`,
+            }}
+          >
+            <RegisterTemplate>
+              <RegisterForm />
+            </RegisterTemplate>
+          </div>
         </div>
       </div>
     );
