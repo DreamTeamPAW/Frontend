07d2ab74c8892d8ac9bac2d2c29c79690ac4e7d0
Adjest <128521507+mpadjest@users.noreply.github.com>
2025-04-14 00:00:19 +0200

    added dummy /recovery page

---
+++ bookbox-frontend/src/app/recovery/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import RecoveryPage from "../../components/04-pages/RecoveryPage";
+import "@/styles/globals.css";
+
+
+
+const Page: React.FC = () => {
+  return <RecoveryPage />;
+};
+
+export default Page;
