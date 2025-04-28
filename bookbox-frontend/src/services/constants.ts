b2827cff9c7192a2a1f54ccef941f9035fb57fbc
Jakub Polski <jakubpolski@gmail.com>
2025-04-28 18:07:55 +0200

    Automatic Azure deployment

--- bookbox-frontend/src/services/constants.ts
+++ bookbox-frontend/src/services/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://api-projektpaw.azurewebsites.net';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001';
 export const LOGIN_URL = '/api/login';
 export const REGISTER_URL = '/api/register';
 export const LOGOUT_URL = '/api/logout';
